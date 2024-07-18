de54975d7e26cf814a82c8cdab5893ef8a146329
kurves <kavssimply@gmail.com>
2024-07-18 22:27:28 +0300

    script to update app schemas

--- app/schemas.py
+++ app/schemas.py
@@ -21,4 +21,33 @@ class PostCreate(BaseModel):
 class Post(BaseModel):
     id: int
     text: str
+    owner_id: int~
+
+
+from pydantic import BaseModel, EmailStr, constr
+from typing import List
+
+class UserBase(BaseModel):
+    email: EmailStr
+
+class UserCreate(UserBase):
+    password: constr(min_length=8)
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class PostBase(BaseModel):
+    text: constr(max_length=1048576)  # 1 MB
+
+class PostCreate(PostBase):
+    pass
+
+class Post(PostBase):
+    id: int
     owner_id: int
+
+    class Config:
+        orm_mode = True
