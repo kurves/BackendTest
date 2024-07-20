9040a2ba1d977da8766fc0ed4c0c63296beac2eb
kurves <kavssimply@gmail.com>
2024-07-20 23:32:11 +0300

    unction to hash passwords

--- app/security.py
+++ app/security.py
@@ -18,6 +18,13 @@ ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
 def get_password_hash(password):
     return pwd_context.hash(password)
 
