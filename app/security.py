d2fe23e9256a3023f9c799a40e06327739317f74
kurves <kavssimply@gmail.com>
2024-07-27 22:20:32 +0300

    update user settings

--- app/security.py
+++ app/security.py
@@ -53,4 +53,21 @@ def authenticate_user(db: Session, email: str, password: str):
 
 
 
+def get_current_user(db: Session = Depends(database.get_db), token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = get_user(db, email=email)
+    if user is None:
+        raise credentials_exception
+    return user
   
