cfe111d46a43426fd3ed222cc9580391b48f6de7
kurves <kavssimply@gmail.com>
2024-07-28 22:34:14 +0300

    user login functionality

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -16,3 +16,17 @@ def signup(user: schemas.UserCreate, db: Session = Depends(database.get_db)):
     db.commit()
     db.refresh(db_user)
     return db_user
+@router.post("/login")
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
+    user = security.authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=security.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = security.create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
