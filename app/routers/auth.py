943a25247ce4a57c0834f48a4321f14f8437fd43
kurves <kavssimply@gmail.com>
2024-07-28 22:33:31 +0300

    user sign up functionaity

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -3,3 +3,16 @@ from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordRequestForm
 from datetime import timedelta
 from .. import models, schemas, security, database
+router = APIRouter()
+
+@router.post("/signup", response_model=schemas.User)
+def signup(user: schemas.UserCreate, db: Session = Depends(database.get_db)):
+    db_user = security.get_user(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    hashed_password = security.get_password_hash(user.password)
+    db_user = models.User(email=user.email, hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
