ce5a1cf91dbd5e4c5db611bbfbeb974f0f06a624
kurves <kavssimply@gmail.com>
2024-07-29 21:46:16 +0300

    get poat functionality

--- app/routers/posts.py
+++ app/routers/posts.py
@@ -13,3 +13,11 @@ def add_post(post: schemas.PostCreate, db: Session = Depends(database.get_db), c
     db.commit()
     db.refresh(db_post)
     return db_post
+
+@router.get("/posts", response_model=list[schemas.Post])
+def get_posts(db: Session = Depends(database.get_db), current_user: models.User = Depends(security.get_current_user)):
+    if current_user.email in cache:
+        return cache[current_user.email]
+    posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
+    cache[current_user.email] = posts
+    return posts
